e2687be3acec594bbdce77f9be92daededb1d6dc
David Fraga Rodriguez <davidfraga92@gmail.com>
2022-11-09 10:17:29 +0100

    Complete ej2

--- mapreduce/webclient_logs/mapper.py
+++ mapreduce/webclient_logs/mapper.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python
 
+
+import os
 import sys
 
+
+ps_objects_counter = 0
+# Obtain file name and format it to get user name
+file_path = os.environ['mapreduce_map_input_file']
+processed_file_path = file_path.split('/')
+file_name = processed_file_path[-1].split('.')[0].replace('con', '')
+
+
 # input comes from STDIN (standard input)
-for line in sys.stdin:
-    # file_name = os.environ['mapreduce_map_input_file']
+for request in sys.stdin:
+    # remove leading and trailing whitespace
+    # split the request into the different request parameters
+    parameters = request.strip().split()
 
-    # us = file_name.split('/')
-    # user = us[-1].split(".")[0][3:]
+    # Access to the request url
+    url = str(parameters[3])
+    # Count each access to an url made by a request
+    print('%s\t%s' % (url, 1))
 
-    process_line = line.strip().split()
-    # increase counters
-    if process_line[3].split('.')[-1] == 'ps"':
-        # Print user and 1 that represent a access to file with .ps
-        # print('%s\t%s' % (user, 1))
-        # Print the URL and 1
-        print('%s\t%s' % (process_line[3], 1))
+    # Access to the object extension extracted
+    # from the url using list comprehension
+    # Checking if the accessed object is a .ps file
+    # If file was .ps extension, increment .ps files accessed by the user
+    # Each file only contains requests from only one user
+    if (url[-4:] == '.ps"'):
+        ps_objects_counter += 1
+
+
+# printing the count of .ps files accessed by each user
+print('%s\t%s' % (file_name, ps_objects_counter))
--- mapreduce/webclient_logs/reducer.py
+++ mapreduce/webclient_logs/reducer.py
@@ -1,28 +1,68 @@
 #!/usr/bin/env python
 
+# Hadoop framework ensures that all values associated
+# with the same key are processed by the same reducer
+# Also, values are sorted alfabetically
+
+
 import sys
 
-current_user = None
-total_user = 0
-user = None
 
+current_url_or_user_id = None
+max_user = ''
+max_url = ''
+url_or_user_id = None
+
+max_user_count = 0
+max_url_count = 0
+current_count = 0
+
+
+# input comes from STDIN
 for line in sys.stdin:
-
+    # remove leading and trailing whitespace
     line = line.strip()
-    array = line.split('\t', 1)
-
-    user = array[0]
+    # parse the input we got from mapper.py
+    url_or_user_id, count = line.split('\t', 1)
     try:
-        count = int(array[1])
+        # try to convert count (currently a string) to int
+        count = int(count)
     except ValueError:
+        # count was not a number, so silently
+        # ignore/discard this line
         continue
 
-    if current_user == user:
-        total_user += count
+    # this IF-switch only works because Hadoop sorts map output
+    # by key (here: url_or_user_id) before it is passed to the reducer
+    # Accumulate data from the same key. When the key changes, keep their
+    # values as a new maximums if they exceeds the current maximum.
+    if current_url_or_user_id == url_or_user_id:
+        current_count += count
     else:
-        if current_user:
-            print '%s\t%s' % (current_user, total_user)
-        current_user = user
-        total_user = count
-if current_user == user:
-    print '%s\t%s' % (current_user, total_user)
+        if current_url_or_user_id and current_url_or_user_id is not None:
+            if '://' in current_url_or_user_id:
+                if (current_count > max_url_count):
+                    max_url = current_url_or_user_id
+                    max_url_count = current_count
+            elif (current_count > max_user_count):
+                max_user = current_url_or_user_id
+                max_user_count = current_count
+
+        current_count = count
+        current_url_or_user_id = url_or_user_id
+
+
+# Last iteration to check the last user/url and its counter
+if current_url_or_user_id == url_or_user_id:
+    if '://' in current_url_or_user_id:
+        if (current_count > max_url_count):
+            max_url = current_url_or_user_id
+            max_url_count = current_count
+    elif (current_count > max_user_count):
+        max_user = current_url_or_user_id
+        max_user_count = current_count
+
+
+# write result to STDOUT
+print('%s\t%s' % (max_user, max_user_count))
+print('%s\t%s' % (max_url, max_url_count))
